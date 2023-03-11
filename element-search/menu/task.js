f12918024d5c4a29d1459d0de30d051efe81929a
korobprog <karobprog@gmail.com>
2023-03-11 14:52:52 +1000

    menu - dz  1 Increased difficulty level

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,19 @@
-
 let menuLink = Array.from(document.getElementsByClassName('menu__link'));
 
-menuLink.forEach(i => i.addEventListener('click', function(evt) {
+menuLink.forEach(i => i.addEventListener('click', function (evt) {
+  closeALLsunMenu();
   if ((i.parentElement.getElementsByClassName('menu_sub')).length === 1) {
     evt.preventDefault();
     let menuSub = Array.from(i.parentElement.getElementsByClassName('menu_sub'));
+    console.log(menuSub)
     menuSub.forEach(sub => sub.classList.toggle('menu_active'));
-    }
+  }
 }));
+
+function closeALLsunMenu() {
+  const subMenu = document.querySelectorAll('.menu_sub');
+  console.log(subMenu)
+  Array.from(subMenu).forEach(i => i.classList.remove('menu_active'));
+}
+
+document.querySelector('.menu_main').onmouseleave = closeALLsunMenu;
