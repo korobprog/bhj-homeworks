2578d6498bd255d48988881c4f6341d620cc9e87
korobprog <karobprog@gmail.com>
2023-03-12 19:08:35 +1000

    menu - dz  1 Increased difficulty level fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -9,11 +9,3 @@ menuLink.forEach(i => i.addEventListener('click', function (evt) {
     menuSub.forEach(sub => sub.classList.toggle('menu_active'));
   }
 }));
-
-function closeALLsunMenu() {
-  const subMenu = document.querySelectorAll('.menu_sub');
-  console.log(subMenu)
-  Array.from(subMenu).forEach(i => i.classList.remove('menu_active'));
-}
-
-document.querySelector('.menu_main').onmouseleave = closeALLsunMenu;
