341d459ef6abbdc5834b68c486c21666a42fcf05
korobprog <korobprog@gmail.com>
2023-04-06 16:51:31 +1000

    fix23

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,34 +1,13 @@
-сonst interestCheck = Array.from(document.getElementsByClassName("interest__check"));
+const interestCheck = [...document.querySelectorAll('.interest__check')];
 
-interestCheck.forEach((element) => {
-  element.addEventListener('change', (event) => {
-    isChecked = event.target.checked;
-    checkedChildren(element, isChecked);
-    checkIndeterminate(element);
-  });
-});
-
-function checkedChildren(element, isChecked) {
-  const li = element.closest('li');
-  const checkedBox = Array.from(li.getElementsByClassName('interest__check'));
-  checkedBox.forEach(elem => elem.checked = isChecked);
-}
-
-function checkIndeterminate(element) {
-  let li = element.closest('li');
-  let ul = null;
-
-  while(li) {
-    ul = li.querySelector('ul');
-    if (ul) {
-      const checkedStatus = Array.from(ul.getElementsByClassName('interest__check'));
-      const parent = li.firstElementChild.querySelector('.interest__check');
-      const some = checkedStatus.some(box => box.checked);
-      const every = checkedStatus.every(box => box.checked);
-
-      parent.checked = every;
-      parent.indeterminate = !every && some;
+interestCheck.forEach((check) => check.addEventListener('change', (event) => {
+    let perent = event.target.closest('.interest');
+    let children = [...perent.querySelectorAll('.interest__check')];
+    
+    if (!children[0].checked) {
+        children.forEach(child => child.checked = false);
+        return
+    } else {
+        children.forEach(child => child.checked = true)
     }
-    li = li.parentElement.closest("li");
-  }
-}
+}))
